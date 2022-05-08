13f858e62a08cdcf23eb9a07652c8ed8c89b63f1
henry-js <79054685+henry-js@users.noreply.github.com>
2022-05-08 23:52:11 +0100

    init commit

---
+++ Model.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace efCoreMigrations;
+internal class BlogContext : DbContext
+{
+    public DbSet<Post> Posts { get; set; } = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite("Data Source=Blogs.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Post>().Property(p => p.Title)
+            .IsRequired();
+    }
+}
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using efCoreMigrations;
+using Spectre.Console;
+using static Spectre.Console.AnsiConsole;
+
+using var db = new BlogContext();
+
+if (!db.Posts.Any())
+{
+    Write(new Markup("[green]Adding sample data...[/]"));
+    db.AddRange(
+        new Post { Title = "Intro to Migrations" },
+        new Post { Title = "Migrations in Team Environments" },
+        new Post { Title = "Advanced Migrations" });
+    db.SaveChanges();
+}
+
+var table = new Table();
+table.AddColumns(nameof(Post.Id), nameof(Post.Title), nameof(Post.Content));
+WriteLine();
+MarkupLine("[bold green]Posts[/]");
+foreach (var post in db.Posts.ToList())
+{
+    // (string Id, string Title, string Content) parsedPost = (post.Id.ToString(), post.Title, post.Content);
+    table.AddRow(post.Id.ToString(), post.Title, post.Content);
+}
+
+Write(table);
+System.Console.ReadLine();
