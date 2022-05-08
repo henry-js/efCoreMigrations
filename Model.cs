04ff751e0ba4f7bf9994108267d9d25ebfaad359
henry-js <79054685+henry-js@users.noreply.github.com>
2022-05-09 00:44:35 +0100

    update

--- Model.cs
+++ Model.cs
@@ -19,5 +19,6 @@ public class Post
 {
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
-    public string Content { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public string? Content { get; set; }
 }
